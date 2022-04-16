f95c21fd766934a607d8276d43ef8b3a894d11dd
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-04-16 06:23:45 +0300

    directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { JosephComponent } from './INTRO TO ANGULAR/joseph/joseph.component';
 import { GroupWorkComponent } from './INTRO TO ANGULAR/group-work/group-work.component';
-import { InterpolationComponent } from './interpolation/interpolation.component';
+import { InterpolationComponent } from './BASICS/Data-Binding/interpolation/interpolation.component';
 import { MainComponent } from './PROJECT-VOTE/quotes-application/main/main.component';
 import { HeaderComponent } from './PROJECT-VOTE/quotes-application/header/header.component';
 import { QuotesComponent } from './PROJECT-VOTE/quotes-application/quotes/quotes.component';
@@ -14,6 +15,8 @@ import { PostQuoteComponent } from './PROJECT-VOTE/quotes-application/post-quote
 
 import { PropertyBindingComponent } from './BASICS/Data-Binding/property-binding/property-binding.component';
 import { EventBindingComponent } from './BASICS/Data-Binding/event-binding/event-binding.component';
+import { TwoWayDataBindingComponent } from './BASICS/Data-Binding/two-way-data-binding/two-way-data-binding.component';
+import { StructuralDirectivesComponent } from './BASICS/Directives/structural-directives/structural-directives.component';
 
 @NgModule({
   declarations: [
@@ -26,10 +29,10 @@ import { EventBindingComponent } from './BASICS/Data-Binding/event-binding/event
     HeaderComponent,
     QuotesComponent,
     PostQuoteComponent,    
-    PropertyBindingComponent, EventBindingComponent
+    PropertyBindingComponent, EventBindingComponent, TwoWayDataBindingComponent, StructuralDirectivesComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule,FormsModule,
     AppRoutingModule
   ],
   providers: [],
